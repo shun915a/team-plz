c8f644b0fbcce8c9f79b738ecafc24bab3492b4f
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-21 17:35:45 +0900

    ユーザー管理機能基礎

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: %i[
+        nickname
+        gender_id
+        birtthday
+        profile
+        twitter_name
+        twitch_name
+        mildom_name
+      ]
+    )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Password must includes letter and number.'
+
+  validates :nickname, uniqueness: true
+
+  with_options presence: true do
+    validates :nickname, uniqueness: true, length: { minimum: 3, maximum: 16, message: '3〜16文字で入力してください。' }
+  end
 end
