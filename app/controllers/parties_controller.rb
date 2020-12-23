57939749573667ac7fb42b7f021abfe42264ed27
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-23 12:10:12 +0900

    new action

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,4 +2,32 @@ class PartiesController < ApplicationController
   def index
     @parties = Party.order('created_at DESC')
   end
+
+  def new
+    @party = Party.new
+  end
+
+  def create
+    @party = Party.new(party_params)
+
+    if @party.save
+      redirect_to parties_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_party
+    @party = Party.find(params[:id])
+  end
+
+  def party_params
+    params.require(:party).permit(
+      :party_title,
+      :party_game_id,
+      :party_text
+    ).merge(user_id: current_user.id)
+  end
 end
