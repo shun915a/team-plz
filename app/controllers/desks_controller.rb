f22a1767a945f27bdd8198f2b5a44ee257ea2bde
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-24 14:58:20 +0900

    デスク投稿

--- app/controllers/desks_controller.rb
+++ app/controllers/desks_controller.rb
@@ -1,2 +1,22 @@
 class DesksController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @desk = Desk.new
+  end
+
+  def create
+    @desk = Desk.new(desk_params)
+
+    if @desk.save
+      redirect_to user_path(@desk.user_id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def desk_params
+    params.require(:desk).permit(:image, :desk_text).merge(user_id: current_user.id)
+  end
 end
