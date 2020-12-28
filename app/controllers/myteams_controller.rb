253e143cd8512555d8c3083cae8db646499d9dcf
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-28 12:20:47 +0900

    routing と controller を設定

--- app/controllers/myteams_controller.rb
+++ app/controllers/myteams_controller.rb
@@ -1,2 +1,23 @@
 class MyteamsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
+
+  def new
+    @myteam = Myteam.new
+  end
+
+  def create
+    @myteam = Myteam.new(myteam_params)
+
+    if @myteam.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def myteam_params
+    params.require(:myteam).permit(:team_name, :team_profile).merge(user_id: current_user.id)
+  end
 end
