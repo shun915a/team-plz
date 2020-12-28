4b92ab662ae78aedaedc33267198d154c457f4c7
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-28 14:31:05 +0900

    show action を実装

--- app/controllers/myteams_controller.rb
+++ app/controllers/myteams_controller.rb
@@ -1,5 +1,6 @@
 class MyteamsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit update destroy]
+  before_action :set_myteam, only: %i[show]
 
   def new
     @myteam = Myteam.new
@@ -15,7 +16,13 @@ class MyteamsController < ApplicationController
     end
   end
 
+  def show; end
+
   private
+
+  def set_myteam
+    @myteam = Myteam.find(params[:id])
+  end
 
   def myteam_params
     params.require(:myteam).permit(:team_name, :team_profile).merge(user_id: current_user.id)
