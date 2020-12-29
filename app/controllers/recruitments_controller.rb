dba427110f976d3911f283983253c91dc793b345
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-29 15:23:50 +0900

    form部分テンプレートを微調整

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,11 +1,8 @@
 class RecruitmentsController < ApplicationController
   def index
-    friends = Friend.order('created_at DESC')
-    parties = Party.order('created_at DESC')
-    teams = Team.order('created_at DESC')
-    scouts = Scout.order('created_at DESC')
-
-    @recruitments = friends.merge(parties).merge(teams).merge(scouts)
-    binding.pry
+    @friends = Friend.order('created_at DESC').limit(5)
+    @parties = Party.order('created_at DESC').limit(5)
+    @teams = Team.order('created_at DESC').limit(5)
+    @scouts = Scout.order('created_at DESC').limit(5)
   end
 end
