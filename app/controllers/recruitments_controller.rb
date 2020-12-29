5c025ec53f0189dff9050ecb659ef5b2196465b6
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-29 15:05:38 +0900

    募集のindexをリファクタリング

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,3 +1,11 @@
 class RecruitmentsController < ApplicationController
-  def index; end
+  def index
+    friends = Friend.order('created_at DESC')
+    parties = Party.order('created_at DESC')
+    teams = Team.order('created_at DESC')
+    scouts = Scout.order('created_at DESC')
+
+    @recruitments = friends.merge(parties).merge(teams).merge(scouts)
+    binding.pry
+  end
 end
