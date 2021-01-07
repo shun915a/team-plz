8627697e2710ddb37f9388fcca9d48ec0c07646b
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-07 15:58:19 +0900

    test RecruitmentModal.js

---
+++ app/javascript/components/RecruitmentModal.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class RecruitmentModal extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div>React Test</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default RecruitmentModal
