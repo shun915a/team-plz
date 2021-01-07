5ad0d08ca605aa53acf7b4f0a2af2c1626f9d56e
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-07 18:40:26 +0900

    remake index by react

--- app/javascript/components/RecruitmentItem.js
+++ app/javascript/components/RecruitmentItem.js
@@ -1,12 +1,34 @@
 import React from "react"
 import PropTypes from "prop-types"
+
 class RecruitmentItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render () {
+    console.log('item loaded')
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <li className="list">
+        <div className="item-title">
+          <h3>{this.props.title}</h3>
+          <span className="category">FRIEND</span>
+        </div>
+
+        <div className="item-info">
+
+          <div className="item-game-id">
+            {this.props.game_id}
+          </div>
+
+          <div className="item-text">
+            {this.props.text}
+          </div>
+        </div>
+      </li>
     );
   }
 }
 
 export default RecruitmentItem
+
+
--- app/javascript/components/RecruitmentList.js
+++ app/javascript/components/RecruitmentList.js
@@ -1,5 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
+import RecruitmentItem from "./RecruitmentItem"
+
 class RecruitmentList extends React.Component {
   render () {
     const posts = this.props.posts
@@ -9,26 +11,18 @@ class RecruitmentList extends React.Component {
     })
 
     return (
-      <React.Fragment>
+        <div className="item-lists">
+
         { posts.map((post) => {
+          return (
+            <RecruitmentItem key = {post.id}
+              title = { post.friend_title }
+              game_id = { post.friend_game_id }
+              text = { post.fiend_text }
+            />
+          )
         })}
-        <li className="list">
-          <div className="item-title">
-            <h3>friend title</h3>
-            <span className="category">FRIEND</span>
-          </div>
-
-          <div className="item-info">
-            <div className="item-game-id">
-              friend_game_id
-            </div>
-
-            <div className="item-text">
-              friend_text
-            </div>
-          </div>
-        </li>
-      </React.Fragment>
+        </div>
     );
   }
 }
