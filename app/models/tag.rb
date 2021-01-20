52aed775378c52428ad6ea407cd99651e50a2b9f
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-20 15:31:46 +0900

    edit view

---
+++ app/models/tag.rb
@@ -0,0 +1,27 @@
+class Tag < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '' },
+    { id: 2, name: '' },
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+    { id: 3, name: '' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :recruitment_tag_relations
+end
