8a9ba4d8ae81fb3971dd3709e2acf198c225c897
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-21 17:48:21 +0900

    fix puma

--- config/puma.rb
+++ config/puma.rb
@@ -7,5 +7,4 @@ plugin :tmp_restart
 app_root = File.expand_path('..', __dir__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
-# pry-railsのbinding.pryが止まる
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
