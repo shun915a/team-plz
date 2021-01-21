909167d2899c8721353828330be429fb5c30c364
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-21 17:47:54 +0900

    fix puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,11 @@
-FROM nginx:1.15.8
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+threads threads_count, threads_count
+port        ENV.fetch('PORT') { 3000 }
+environment ENV.fetch('RAILS_ENV') { 'development' }
+plugin :tmp_restart
 
-RUN rm -f /etc/nginx/conf.d/*
+app_root = File.expand_path('..', __dir__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
-ADD nginx.conf /etc/nginx/conf.d/team-plz.conf
-
-# ビルド完了後にNginxを起動
-CMD /usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
+# pry-railsのbinding.pryが止まる
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
