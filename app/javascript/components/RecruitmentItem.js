3bb170b722ccf3f606e35d42ee70d72c500652d3
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-23 17:01:04 +0900

    Update RecruitmentItem.js

--- app/javascript/components/RecruitmentItem.js
+++ app/javascript/components/RecruitmentItem.js
@@ -3,6 +3,7 @@ import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+<<<<<<< HEAD
 
 
 
@@ -10,12 +11,50 @@ class RecruitmentItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {isModalOpen: false};
+=======
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+
+const modalStyle = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    backgroundColor: "rgba(0,0,0,0.85)"
+  },
+  content: {
+    backgroundColor: "#00000000",
+    border: 'none'
+  }
+};
+
+export default function RecruitmentItem(props) {
+
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  let editButton;
+  if (props.currentUserId == props.postUserId) {
+    editButton = (
+      <Button 
+        variant="contained"
+        color="secondary" 
+        size="large"
+        startIcon={<EditIcon />}
+        href={`/${props.editUrl}/${props.id}/edit`}
+      >
+        EDIT
+      </Button>
+    )
+>>>>>>> parent of 47ca88f... add animation and fix to modal
   }
 
   handleClickRecruitment() {
     this.setState({isModalOpen: true});
   }
 
+<<<<<<< HEAD
   handleClickClose() {
     this.setState({isModalOpen: false});
   }
@@ -76,6 +115,38 @@ class RecruitmentItem extends React.Component {
                 {this.props.title}
               </div>
 
+=======
+  const tagProps = props.tags;
+  const tagList = tagProps.map((tag) =>
+    <span className="tag">{tag.name}</span>
+  );
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    if (modalIsOpen){
+      document.body.style.overflow = 'hidden';
+    } 
+
+    if (!modalIsOpen){
+      document.body.style.overflow = 'unset';
+    } 
+  });
+
+
+
+  let modal = (
+    <Modal isOpen={modalIsOpen} style={modalStyle}>
+        <div className='modal-inner'>
+          <div className='modal-header'>{props.category}</div>
+          <div className='modal-introduction'>
+
+            <div className='modal-item-title'>
+              <p className='label-text'>
+                TITLE:
+              </p>
+              {props.title}
+            </div>
+>>>>>>> parent of 47ca88f... add animation and fix to modal
 
               <div className='modal-item-game-id'>
                 <p className='label-text'>NAME:</p>
