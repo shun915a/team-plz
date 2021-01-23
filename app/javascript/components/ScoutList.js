0b62fe80fb42e3876e92eda97b9905a2402420c8
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-23 13:35:03 +0900

    fix components

--- app/javascript/components/ScoutList.js
+++ app/javascript/components/ScoutList.js
@@ -2,26 +2,22 @@ import React from "react"
 import RecruitmentItem from "./RecruitmentItem"
 
 
-class ScoutList extends React.Component {
-  render () {
-    const post = this.props.post
-    const tags = this.props.tags
-    const userId = this.props.userId
+export default function ScoutList(props) {
+  const post = props.post
+  const tags = props.tags
+  const userId = props.userId
 
-    return (
-        <RecruitmentItem key = {post.id}
-          category = "SCOUT"
-          editUrl = "scouts"
-          id = { post.id }
-          currentUserId = { userId }
-          postUserId = { post.user_id }
-          title = { post.scout_title }
-          gameId = { post.scout_game_id }
-          tags = { tags }
-          text = { post.scout_text }
-        />
-    );
-  }
+  return (
+    <RecruitmentItem key = {post.id}
+      category = "SCOUT"
+      editUrl = "scouts"
+      id = { post.id }
+      currentUserId = { userId }
+      postUserId = { post.user_id }
+      title = { post.scout_title }
+      gameId = { post.scout_game_id }
+      tags = { tags }
+      text = { post.scout_text }
+    />
+  );
 }
-
-export default ScoutList
