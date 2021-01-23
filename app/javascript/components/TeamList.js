1a7e11706c9e697a86998ba14473eb4eb78fd43c
shun915a <74646604+shun915a@users.noreply.github.com>
2021-01-23 16:59:58 +0900

    Revert "Merge pull request #28 from shun915a/modal-animation"
    This reverts commit 304c1ef5e55e811189a0b6c9511e2383a46453c4, reversing
    changes made to 7df4f7c80cd77f6d2aeacf02ba842b5aa1d195f1.

--- app/javascript/components/TeamList.js
+++ app/javascript/components/TeamList.js
@@ -2,23 +2,26 @@ import React from "react"
 import RecruitmentItem from "./RecruitmentItem"
 
 
-export default function(props) {
-  const post = props.post
-  const tags = props.tags
-  const userId = props.userId
+class TeamList extends React.Component {
+  render () {
+    const post = this.props.post
+    const tags = this.props.tags
+    const userId = this.props.userId
 
-  return (
-    <RecruitmentItem key = {post.id}
-      category = "TEAM"
-      editUrl = "teams"
-      id = { post.id }
-      currentUserId = { userId }
-      postUserId = { post.user_id }
-      title = { post.team_title }
-      gameId = { post.team_game_id }
-      tags = { tags }
-      text = { post.team_text }
-    />
-  );
+    return (
+        <RecruitmentItem key = {post.id}
+          category = "TEAM"
+          editUrl = "teams"
+          id = { post.id }
+          currentUserId = { userId }
+          postUserId = { post.user_id }
+          title = { post.team_title }
+          gameId = { post.team_game_id }
+          tags = { tags }
+          text = { post.team_text }
+        />
+    );
+  }
 }
 
+export default TeamList
