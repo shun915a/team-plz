a5222aa9e91544d4d6675acd5e944ed76be4126b
shun915a <74646604+shun915a@users.noreply.github.com>
2021-02-25 16:00:16 +0900

    rspec model test

--- spec/factories/party_members.rb
+++ spec/factories/party_members.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :party_member do
-    
+    association :user
+    association :party
+    role { Faker::Number.between(from: 0, to: 2) }
   end
 end
--- spec/models/party_member_spec.rb
+++ spec/models/party_member_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe PartyMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @party_member = FactoryBot.create(:party_member)
+    end
+
+    context '投稿成功' do
+      it 'user_id, party_id, roleがあれば登録できる' do
+        expect(@party_member).to be_valid
+      end
+    end
+
+    context 'エラー発生時' do
+      it 'user_idが空では登録できない' do
+        @party_member.user_id = nil
+        @party_member.valid?
+        expect(@party_member.errors.full_messages).to include('User must exist')
+      end
+      it 'party_idが空では登録できない' do
+        @party_member.party_id = nil
+        @party_member.valid?
+        expect(@party_member.errors.full_messages).to include('Party must exist')
+      end
+    end
+  end
 end
