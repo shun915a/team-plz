68d20ba53357141402cf67096eccc6357c7878ac
shun915a <74646604+shun915a@users.noreply.github.com>
2021-02-28 10:06:05 +0900

    remove presence validations

--- app/models/party_member.rb
+++ app/models/party_member.rb
@@ -5,11 +5,6 @@ class PartyMember < ApplicationRecord
   enum role: { free: 0, manager: 1, member: 2 }
   enum status: { request: 0, accept: 1, decline: 2 }
 
-  with_options presence: true do
-    validates :user_id
-    validates :party_id
-  end
-
   def accept_request
     if request?
       ApplicationRecord.transaction do
